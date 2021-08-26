574f89a4be9f342bde289ac1ee86a284bef9941b
Alejandra Diaz <alejandradp19@gmail.com>
2021-08-25 21:23:49 -0500

    Implemented functionality to read the csv file, and save the registers in a list. It is relevant to point that the info saved in the list is displayed in a label in the interface. The table for the visualization has not been implemented yet.

--- Tarea4/MainWindow.xaml.cs
+++ Tarea4/MainWindow.xaml.cs
@@ -12,17 +12,135 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Tarea4
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
+    public class Register
+    {
+        private string codigodep, codigomun, departamento, municipio, tipo;
+
+        public Register(string codigodep, string codigomun, string departamento, string municipio, string tipo)
+        {
+            this.codigodep = codigodep;
+            this.codigomun = codigomun;
+            this.departamento = departamento;
+            this.municipio = municipio;
+            this.tipo = tipo;
+        }
+
+        public String Codigodep
+        {
+            get
+            {
+                return codigodep;
+            }
+            set
+            {
+                codigodep = value;
+            }
+        }
+
+
+        public String Codigomun
+        {
+            get
+            {
+                return codigomun;
+            }
+            set
+            {
+                codigomun = value;
+            }
+        }
+
+        public String Municipio
+        {
+            get
+            {
+                return municipio;
+            }
+            set
+            {
+                municipio = value;
+            }
+        }
+
+        public String Tipo
+        {
+            get
+            {
+                return tipo;
+            }
+            set
+            {
+                tipo = value;
+            }
+        }
+
+        public String toString()
+        {
+            return codigodep + " " + codigomun + " " + departamento + " " + municipio + " " + tipo;
+        }
+
+    }
+
     public partial class MainWindow : Window
     {
+        public List<Register> registers = new List<Register>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("C:\\Users\\aleja\\Desktop\\ICESI\\SEMESTRE_7\\PROYECTO_INTEGRADOR_I\\ProyectosVS\\Test\\Codigos_municipios.csv");
+                //Read the first line of text
+
+                line = sr.ReadLine();
+
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    var values = line.Split(',');
+                    Register r = new Register(values[0], values[1], values[2], values[3], values[4]);
+                    registers.Add(r);
+                    //lstNames.Content += r.toString() + "\n";
+                    //write the lie to console window
+                    //lstNames.Content += line + "\n";
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+
+
+                int i = 1;
+                while (registers.ElementAt(i) != null)
+                {
+                    lstNames.Content += registers.ElementAt(i).toString() + "\n";
+                    i++;
+                }
+
+
+                //close the file
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
         }
     }
 }
