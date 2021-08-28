be4fa46a981992c07c1c040697bd43133bfb7274
Alejandra Diaz <alejandradp19@gmail.com>
2021-08-28 16:23:22 -0500

    Separated the class MainWindow.caml.cs so it only has the graphical part, and the logical part is now in the Principal class.

--- Tarea4/MainWindow.xaml.cs
+++ Tarea4/MainWindow.xaml.cs
@@ -25,54 +25,26 @@ namespace Tarea4
 
     public partial class MainWindow : Window
     {
-        private List<Registro> registers = new List<Registro>();
+        private Principal principal;
+       // private List<Registro> registers = new List<Registro>();
 
         public MainWindow()
         {
             InitializeComponent();
 
-            String line;
-            try
+            principal = new Principal();
+
+            showRegisters();
+        }
+
+        public void showRegisters()
+        {
+            int i = 1;
+            ;
+            while (i<principal.getAllRegistros().Count())
             {
-                //Pass the file path and file name to the StreamReader constructor
-                StreamReader sr = new StreamReader("C:\\Users\\aleja\\Desktop\\ICESI\\SEMESTRE_7\\PROYECTO_INTEGRADOR_I\\ProyectosVS\\Test\\Codigos_municipios.csv");
-                //Read the first line of text
-
-                line = sr.ReadLine();
-
-                //Continue to read until you reach end of file
-                while (line != null)
-                {
-                    var values = line.Split(',');
-                    Registro r = new Registro(values[0], values[1], values[2], values[3], values[4]);
-                    registers.Add(r);
-                    //lstNames.Content += r.toString() + "\n";
-                    //write the lie to console window
-                    //lstNames.Content += line + "\n";
-                    //Read the next line
-                    line = sr.ReadLine();
-                }
-
-
-                int i = 1;
-                while (registers.ElementAt(i) != null)
-                {
-                    lstNames.Content += registers.ElementAt(i).toString() + "\n";
-                    i++;
-                }
-
-
-                //close the file
-                sr.Close();
-                Console.ReadLine();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Exception: " + e.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Executing finally block.");
+                lstNames.Content += principal.getAllRegistros().ElementAt(i).toString() + "\n";
+                i++;
             }
         }
     }
---
+++ Tarea4/Principal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea4
+{
+    class Principal
+    {
+        private List<Registro> registros = new List<Registro>();
+        public Principal() {
+            cargarArchivo();
+        }
+
+        public void cargarArchivo()
+        {
+            String line;
+            try
+            {
+                //Pass the file path and file name to the StreamReader constructor
+                StreamReader sr = new StreamReader("C:\\Users\\aleja\\Desktop\\ICESI\\SEMESTRE_7\\PROYECTO_INTEGRADOR_I\\ProyectosVS\\Test\\Codigos_municipios.csv");
+                //Read the first line of text
+
+                line = sr.ReadLine();
+
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    string[] values = line.Split(',');
+                    Registro r = new Registro(values[0], values[1], values[2], values[3], values[4]);
+                    registros.Add(r);
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+
+                //close the file
+                sr.Close();
+                //Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+
+        public List<Registro> getAllRegistros(){
+            return registros;
+        }
+    }
+}
